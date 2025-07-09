28ce7118ca8638e8ac4d3c17e451ed998c47ea75
Samima Nasrin <sn5300167@gmail.com>
2025-07-09 15:30:47 +0530

    Initial commit

---
+++ client/src/components/HeroSection.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import { Button } from './ui/button'
+import { Search } from 'lucide-react'
+import { useDispatch } from 'react-redux';
+import { setSearchedQuery } from '@/redux/jobSlice';
+import { useNavigate } from 'react-router-dom';
+
+const HeroSection = () => {
+    const [query, setQuery] = useState("");
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const searchJobHandler = () => {
+        dispatch(setSearchedQuery(query));
+        navigate("/browse");
+    }
+
+    return (
+        <div className='text-center'>
+            <div className='flex flex-col gap-5 my-10'>
+                <span className=' mx-auto px-4 py-2 rounded-full bg-white-100 text-[#F83002] font-medium'></span>
+                <h1 className='text-5xl font-bold'>UNLOCK <br /><span className='text-[#002C58]'>YOUR CAREER</span></h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia et nobis tenetur repellendus ducimus aspernatur iste! Temporibus corporis deleniti.</p>
+                <div className='flex w-[40%] shadow-lg border border-gray-200 pl-3 rounded-full items-center gap-4 mx-auto'>
+                    <input
+                        type="text"
+                        placeholder='Search Opportunities'
+                        onChange={(e) => setQuery(e.target.value)}
+                        className='outline-none border-none w-full'
+
+                    />
+                    <Button onClick={searchJobHandler} className="rounded-r-full bg-[#002C58]">
+                        <Search className='h-5 w-5' />
+                    </Button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HeroSection
